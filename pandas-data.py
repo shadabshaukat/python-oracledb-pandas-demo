9a125bb5ab20dd945337935fb386139f976dfa86
Shadab Mohammad <39692236+shadabshaukat@users.noreply.github.com>
2023-03-13 16:50:22 +1100

    Create pandas-data.py

---
+++ pandas-data.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+import pandas as pd
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+
+import oracledb
+
+oracledb.version = "8.3.0"
+sys.modules["cx_Oracle"] = oracledb
+
+un = os.environ.get("PYTHON_USERNAME")
+pw = os.environ.get("PYTHON_PASSWORD")
+dsn = os.environ.get("PYTHON_CONNECTSTRING")  #connection string
+
+engine_cloud = create_engine(f'oracle://{un}:{pw}@{dsn}')
+
+try:
+   # Read employees table
+   employees_sql = "SELECT * FROM employees"
+   df_employees = pd.read_sql(employees_sql,engine_cloud)
+   print(df_employees)
+
+   # Read employees_salary table
+   employees_salary_sql = "SELECT * FROM employees_salary"
+   df_employees_salary = pd.read_sql(employees_salary_sql, engine_cloud)
+   print(df_employees_salary)
+
+   print("")
+   print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+   print("Statistical Analysis of Bonus and Salary for Employees")
+   print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+   print("")
+
+   # Avergae Salaries by Department
+   merged_df = pd.merge(df_employees_salary,df_employees, on='id')
+   avg_salaries = merged_df.groupby('department')['salary'].mean()
+   print("+++++++++++++++++++++++++++++++")
+   print("Avergae Salaries Per Department")
+   print("+++++++++++++++++++++++++++++++")
+   print(avg_salaries)
+
+   # Average Bonus by Department
+   avg_bonuses = merged_df.groupby('department')['bonus'].mean()
+   print("++++++++++++++++++++++++++++")
+   print("Avergae Bonus Per Department")
+   print("++++++++++++++++++++++++++++")
+   print(avg_bonuses)
+
+   # Get the mean, median, standard deviation, and other statistics for the salary column in df_employees_salary
+   salary_stats = df_employees_salary['salary'].describe()
+   print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+   print("Mean, median, standard deviation, and other statistics for Salary")
+   print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+   print(salary_stats)
+
+   # Calculate the correlation matrix between the salary and bonus columns in df_employees_salary
+   corr_matrix = df_employees_salary[['salary', 'bonus']].corr()
+   print("+++++++++++++++++++++++++++++++++++++++++++++++")
+   print("Correlation matrix between the salary and bonus")
+   print("+++++++++++++++++++++++++++++++++++++++++++++++")
+   print(corr_matrix)
+
+
+except SQLAlchemyError as e:
+   print(e)
